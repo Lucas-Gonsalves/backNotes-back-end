0fee710a973d6fbc9a3b18829453685f6805b2ee
lucas <lucasluz1710@gmail.com>
2023-09-06 13:39:51 -0300

    Created file database and connectionDatabase

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
+require("express-async-errors");
 const express = require("express");
 const routes = require("./routes");
+const connectionDatabase = require("./database/sqlite");
 
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
-
+connectionDatabase()
 
 const PORT = 3333;
 app.listen(PORT, () => { console.log(`Server is runing on port: ${PORT}.`) } );
